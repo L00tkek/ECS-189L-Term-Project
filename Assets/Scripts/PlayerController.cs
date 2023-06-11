50742fe5ad39a5a66a51cdbc6227e5d568ab7383
L00tkek <rutvikdeo@gmail.com>
2023-06-10 18:38:57 -0700

    Player Movement.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    float walkSpeed = 10f;
+    float inputHorizontal;
+    float inputVertical;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inputHorizontal = Input.GetAxisRaw("Horizontal");
+        inputVertical = Input.GetAxisRaw("Vertical");
+    }
+
+    // physics-related movement goes here
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(inputHorizontal, inputVertical).normalized * walkSpeed;
+    }
+}
