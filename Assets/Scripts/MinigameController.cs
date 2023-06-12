d02b7bca908b97ad5fce03041a84971b652050fd
Lucy Robillard <larobillard@ucdavis.edu>
2023-06-12 14:50:44 -0700

    Create the begginings of a minigame. You must complete said minigame in order to complete a task.
    The Unity Editor doesn't display any of the fields of scripts on my
    little laptop, so I will have to go to my desktop machine to finish it.

---
+++ Assets/Scripts/MinigameController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using TMPro;
+
+public class MinigameController : MonoBehaviour
+{
+	[SerializeField] TextMeshProUGUI displayText;
+	[SerializeField] Button[] buttons;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
