2ebb616b8205e4a96f301113544482ea727147b4
L00tkek <rutvikdeo@gmail.com>
2023-06-13 15:14:04 -0700

    Adding keyboard controls for the minigame.

--- Assets/Scripts/MinigameController.cs
+++ Assets/Scripts/MinigameController.cs
@@ -125,6 +125,47 @@ public class MinigameController : MonoBehaviour
                 displayNextNum();
             }
         }
+
+        if (Input.GetKeyDown(KeyCode.Keypad1) || Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            buttonPressed(1);
+        }
+        else if (Input.GetKeyDown(KeyCode.Keypad2) || Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            buttonPressed(2);
+        }
+        else if (Input.GetKeyDown(KeyCode.Keypad3) || Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            buttonPressed(3);
+        }
+        else if (Input.GetKeyDown(KeyCode.Keypad4) || Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            buttonPressed(4);
+        }
+        else if (Input.GetKeyDown(KeyCode.Keypad5) || Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            buttonPressed(5);
+        }
+        else if (Input.GetKeyDown(KeyCode.Keypad6) || Input.GetKeyDown(KeyCode.Alpha6))
+        {
+            buttonPressed(6);
+        }
+        else if (Input.GetKeyDown(KeyCode.Keypad7) || Input.GetKeyDown(KeyCode.Alpha7))
+        {
+            buttonPressed(7);
+        }
+        else if (Input.GetKeyDown(KeyCode.Keypad8) || Input.GetKeyDown(KeyCode.Alpha8))
+        {
+            buttonPressed(8);
+        }
+        else if (Input.GetKeyDown(KeyCode.Keypad9) || Input.GetKeyDown(KeyCode.Alpha9))
+        {
+            buttonPressed(9);
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.Backspace))
+        {
+            cancelPressed();
+        }
     }
 
     void Awake()
